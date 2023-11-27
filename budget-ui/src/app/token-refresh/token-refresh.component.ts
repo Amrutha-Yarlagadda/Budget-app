fbc7859a21af6f333f472fefb3b6dab8405fc722
Amrutha <amrutha1827@gmail.com>
2023-11-26 23:50:32 -0500

    adding

---
+++ budget-ui/src/app/token-refresh/token-refresh.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-token-refresh',
+  templateUrl: './token-refresh.component.html',
+  styleUrls: ['./token-refresh.component.scss']
+})
+export class TokenRefreshComponent {
+
+}
